1a35b3a1a3825cc5f9720cc9e2272bb2fbc275f2
Igor Kruglyak <igorkruglyak@gmail.com>
2020-06-16 15:09:03 +0300

    Function for reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,33 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.33
+# './Data/portfolio.csv'
 
-total_cost = 0.0
 
-with open('./Data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total_cost =+ nshares * price
-print(f'Total cost: {total_cost}')
+import csv
+def portfolio_cost(filename):
+    '''
+    Computes the total cost (shares*price) of a portfolio file
+    '''
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost =+ nshares * price
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print('Bad row:', row)
+    return total_cost
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print(f'Total cost: {cost}')
